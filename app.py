9fc76ee387ffe3b75e8cc770bf471509963849fb
AustinS <18nitsua@gmail.com>
2024-02-03 12:49:40 -0600

    Update to force login

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-# This example is based on this authlib tutorial: 
+# This example is based on this authlib tutorial:
 # https://github.com/authlib/demo-oauth-client/blob/master/fastapi-google-login/app.py
 
 import json
@@ -23,55 +23,56 @@ config = Config()
 oauth = OAuth(config)
 
 
-CONF_URL = server_metadata_url=f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+CONF_URL = server_metadata_url = (
+    f'https://{env.get("AUTH0_DOMAIN")}/.well-known/openid-configuration'
+)
 oauth.register(
-    name='auth0',
+    name="auth0",
     client_id=env.get("AUTH0_CLIENT_ID"),
     client_secret=env.get("AUTH0_CLIENT_SECRET"),
     server_metadata_url=CONF_URL,
-    client_kwargs={
-        'scope': 'openid email profile'
-    }
+    client_kwargs={"scope": "openid email profile"},
 )
 
 
-@app.get('/')
+@app.get("/")
 async def homepage(request: Request):
-    user = request.session.get('user')
+    user = request.session.get("user")
     if user:
         data = json.dumps(user)
-        html = (
-            f'<pre>{data}</pre>'
-            '<a href="/logout">logout</a>'
-        )
+        html = f"<pre>{data}</pre>" '<a href="/logout">logout</a>'
         return HTMLResponse(html)
-    return HTMLResponse('<a href="/login">login</a>')
-
-
-@app.get('/login')
-async def login(request: Request):
-    redirect_uri = request.url_for('auth')
+    # return HTMLResponse('<a href="/login">login</a>')
+    redirect_uri = request.url_for("auth")
     return await oauth.auth0.authorize_redirect(request, redirect_uri)
 
 
-@app.get('/auth')
+@app.get("/login")
+async def login(request: Request):
+    redirect_uri = request.url_for("auth")
+    return await oauth.auth0.authorize_redirect(request, redirect_uri)
+
+
+@app.get("/auth")
 async def auth(request: Request):
     try:
         token = await oauth.auth0.authorize_access_token(request)
     except OAuthError as error:
-        return HTMLResponse(f'<h1>{error.error}</h1>')
-    user = token.get('userinfo')
+        return HTMLResponse(f"<h1>{error.error}</h1>")
+    user = token.get("userinfo")
     if user:
-        request.session['user'] = dict(user)
-    return RedirectResponse(url='/')
+        request.session["user"] = dict(user)
+    return RedirectResponse(url="/")
 
 
-@app.get('/logout')
+@app.get("/logout")
 async def logout(request: Request):
-    request.session.pop('user', None)
-    return RedirectResponse(url='/')
+    request.session.pop("user", None)
+    # return RedirectResponse(url="/")
+    return HTMLResponse('<h1>You have been logged out</h1><a href="/login">login</a>')
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+
+    uvicorn.run(app, host="127.0.0.1", port=8000)
